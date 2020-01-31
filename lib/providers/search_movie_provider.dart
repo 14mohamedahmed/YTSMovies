1499d4b10b7879af0a70e3e3bc534dd31dc25ab6
wmfadel <mohamed.fadel24@hotmail.com>
2020-01-31 19:13:02 +0200

    fixed search 🔍🔍

--- lib/providers/search_movie_provider.dart
+++ lib/providers/search_movie_provider.dart
@@ -6,17 +6,21 @@ import 'package:http/http.dart' as h;
 import 'package:ytsm/models/movie.dart';
 
 class SearchMovieProvider extends ChangeNotifier {
-  List<Movie> _movie;
-  List<Movie> get movie {
-    return _movie;
+  List<Movie> _movies = [];
+  List<Movie> get movies {
+    return _movies;
   }
 
   Future<void> searchMovie(String queryTerm) async {
-    final String url = Environment.moviesUrl+'query_term=$queryTerm';
+    final String url = Environment.moviesUrl + '?query_term=$queryTerm';
     h.Response response = await h.get(url);
-    final res = json.decode(response.body) as Map<String,dynamic>;
-    _movie.add(Movie.fromJson(res));
-    print(_movie);
+    Map<String, dynamic> res = json.decode(response.body);
+    if (res['data']['movie_count'] < 1) return;
+    _movies.clear();
+    for (var movie in res['data']['movies']) {
+      _movies.add(Movie.fromJson(movie));
+    }
+    print(_movies.length);
     notifyListeners();
   }
 }
